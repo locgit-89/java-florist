b1cda4acad7399896c6aefd1c57121320c7f6eb3
TrungAnh.Tran <trunganh.tran369@gmail.com>
2025-11-13 18:48:02 +0700

    tui sửa

--- backend/JavaFlorist/JavaFlorist.Api/Data/huyTest.cs
+++ backend/JavaFlorist/JavaFlorist.Api/Data/huyTest.cs
@@ -4,7 +4,7 @@ namespace JavaFlorist.Api.Data
     public class Vidu
     {
         public int a = 10;
-        public int ham() {
+        public int ham1212121() {
             return a;
         }
     }
