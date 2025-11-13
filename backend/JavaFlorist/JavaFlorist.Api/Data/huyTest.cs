e0a597f81729cb8700db807c0b2b8467248686e5
locgit-89 <thantheloc@gmail.com>
2025-11-13 18:45:25 +0700

    them ham

--- backend/JavaFlorist/JavaFlorist.Api/Data/huyTest.cs
+++ backend/JavaFlorist/JavaFlorist.Api/Data/huyTest.cs
@@ -3,7 +3,9 @@ namespace JavaFlorist.Api.Data
 {
     public class Vidu
     {
-        //6gio10 ngay 13 thang 11 nam 2025
-        // rrr
+        public int a = 10;
+        public int ham() {
+            return a;
+        }
     }
 }
